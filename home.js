5951727a66fb1e975ee48821495b29274fedee5e
Nejla S <112932190+nejla8@users.noreply.github.com>
2024-06-08 16:25:21 +0200

    Create home.js
    update project 3

---
+++ home.js
@@ -0,0 +1,14 @@
+// components/Home.js
+import React from 'react';
+import { Typography } from '@mui/material';
+
+function Home() {
+  return (
+    <div>
+      <Typography variant="h2">Welcome to Owl Coffee</Typography>
+      <Typography variant="body1">Would you like to start the day with a cup of nice coffee?</Typography>
+    </div>
+  );
+}
+
+export default Home;
